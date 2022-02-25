9845812e468bae4bb88b902cef2b94ff6d1cc8c4
sidharth sahni <54073655+deevo-sage@users.noreply.github.com>
2022-02-26 02:07:35 +0530

    init

---
+++ packages/hardhat/contracts/YourContract.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// import "@openzeppelin/contracts/access/Ownable.sol";
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+
+contract YourContract is ERC20 {
+    event SetPurpose(address sender, string purpose);
+
+    string public purpose = "Building Unstoppable Apps!!!";
+
+    constructor() payable ERC20("MyToken", "MTK") {
+        // what should we do on deploy?
+    }
+
+    function setPurpose(string memory newPurpose) public {
+        purpose = newPurpose;
+        console.log(msg.sender, "set purpose to", purpose);
+        emit SetPurpose(msg.sender, purpose);
+    }
+
+    // to support receiving ETH by default
+    receive() external payable {}
+
+    fallback() external payable {}
+}
